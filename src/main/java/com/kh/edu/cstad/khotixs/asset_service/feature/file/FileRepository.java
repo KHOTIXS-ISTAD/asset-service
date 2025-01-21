5074188bf841437c4bc0eef19e83c4c62c680eae
soknem <sokname56@gmail.com>
2025-01-21 13:48:29 +0700

    Auto commit

--- src/main/java/com/kh/edu/cstad/khotixs/asset_service/feature/file/FileRepository.java
+++ src/main/java/com/kh/edu/cstad/khotixs/asset_service/feature/file/FileRepository.java
@@ -2,6 +2,8 @@ package com.kh.edu.cstad.khotixs.asset_service.feature.file;
 
 import com.kh.edu.cstad.khotixs.asset_service.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
