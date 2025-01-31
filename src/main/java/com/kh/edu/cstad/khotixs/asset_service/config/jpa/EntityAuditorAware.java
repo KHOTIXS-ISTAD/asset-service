00672c863d2483f2317e70daa1aa082cf6d87229
soknem <sokname56@gmail.com>
2025-01-31 14:53:04 +0700

    Auto commit

--- src/main/java/com/kh/edu/cstad/khotixs/asset_service/config/jpa/EntityAuditorAware.java
+++ src/main/java/com/kh/edu/cstad/khotixs/asset_service/config/jpa/EntityAuditorAware.java
@@ -1,24 +1,28 @@
 package com.kh.edu.cstad.khotixs.asset_service.config.jpa;
 
-import org.jetbrains.annotations.NotNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import java.util.Optional;
 
+@Slf4j
 public class EntityAuditorAware implements AuditorAware<String> {
-    @NotNull
+
     @Override
     public Optional<String> getCurrentAuditor() {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        if (auth == null || !(auth.getPrincipal() instanceof UserDetails user)) {
+        if (auth==null|| !(auth instanceof JwtAuthenticationToken jwtAuth)) {
             return Optional.of("KHOTIXS");
         }
 
-        return Optional.of(user.getUsername());
+        String uuid = jwtAuth.getToken().getClaimAsString("uuid");
+
+        return Optional.ofNullable(uuid).or(() -> Optional.of("KHOTIXS"));
     }
 }
